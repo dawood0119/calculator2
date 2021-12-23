f8174069ec227048534ec87972fc0b224cc15c46
dawood0119 <dawoodyousuf103@gmail.com>
2021-12-23 17:21:06 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let result = document.getElementById("inputText");
+
+let calculate = (number)=>{
+    result.value+=number;
+}
+let Result=()=>{
+    try{
+        result.value = eval(result.value);
+    }
+    catch(err){
+        alert("Enter valid input");
+    }
+}
+function clr(){
+    result.value = "";
+}
+
+function del(){
+    result.value=result.value.slice(0,-1);
+}
